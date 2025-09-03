5d6cff1dd96802dc9fa951f79b7e996b89e574e7
Gabriel Couto <gabriel.couto2704@gmail.com>
2025-09-03 14:18:33 -0300

    Adicionando novos exercícios do 11 ao 15

--- Checkpoint1_Java_010925.java
+++ Checkpoint1_Java_010925.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class Checkpoint1_010925 {
+public class Checkpoint1_Java_010925 {
     static Scanner input = new Scanner(System.in);
 
     public static class ExerciciosJava1 {
@@ -146,8 +146,6 @@ public class Checkpoint1_010925 {
             //•	18,5 até 24,9 → Peso normal
             //•	25 até 29,9 → Sobrepeso
             //•	30 ou mais → Obesidade
-            // imc = peso (kg) / altura (m) ** 2
-
             System.out.println("Digite seu peso: ");
             double peso = input.nextDouble();
             System.out.println("Digite sua altura: ");
@@ -190,7 +188,6 @@ public class Checkpoint1_010925 {
             //•	À vista (desconto de 10%)
             //•	Parcelado em 2x (sem juros)
             //•	Parcelado em 4x (juros de 8%)
-
             System.out.println("Insira o preço do produto: ");
             double valorProduto = input.nextDouble();
             System.out.println("Indique a forma de pagamento (1 - à vista (10% de desconto), 2 - Parcelado em 2x sem juros, 3 - Parcelado em 4x com 8% de juros)");
@@ -200,6 +197,104 @@ public class Checkpoint1_010925 {
                 case 2 -> System.out.println("O valor final é de R$" + valorProduto / 2 + " ao mês por dois meses (sem juros).");
                 case 3 -> System.out.println("O valor final é de R$" + (valorProduto + (valorProduto * 0.08)) / 4 + " ao mês por 4 meses com 8% de juros ao todo.");
                 default -> System.out.println("Opção não reconhecida.");
+            }
+        }
+    }
+
+    public static class exercicio11 {
+        public static void main(String[] args) {
+            // Exercício 11: Crie um programa que leia o nome e a idade de 3 pessoas. Ao final, mostre o nome da pessoa mais velha.
+            String nomeMaisVelho = "";
+            int idadeMaisVelho = 0;
+
+            for (int i = 1; i <= 3; i++) {
+                System.out.print(i + "° Digite seu nome: ");
+                String nome = input.next();
+
+                System.out.print(i + "° Digite sua idade: ");
+                int idade = input.nextInt();
+
+                System.out.println("");
+
+                if (i == 1 || idade > idadeMaisVelho) {
+                    idadeMaisVelho = idade;
+                    nomeMaisVelho = nome;
+                }
+            }
+            System.out.println("\nA pessoa mais velha é " + nomeMaisVelho);
+        }
+    }
+
+    public static class exercicio12 {
+        public static void main(String[] args) {
+            // Exercício 12: Escreva um programa que leia a altura, o comprimento e a largura de uma caixa, e calcule o seu volume
+            System.out.print("Digite a altura da caixa: ");
+            double altura = input.nextDouble();
+
+            System.out.print("Digite o comprimento da caixa: ");
+            double comprimento = input.nextDouble();
+
+            System.out.print("Digite a largura da caixa: ");
+            double largura = input.nextDouble();
+
+            double volume = altura * comprimento * largura;
+
+            System.out.print("A caixa tem " + volume + " de volume.");
+        }
+    }
+
+    public static class exercicio13 {
+        // Exercício 13: Crie uma classe chamada Car com os atributos: brand, model, year. No main, crie dois objetos da classe e exiba as informações.
+        public static class Car {
+            String brand;
+            String model;
+            int year;
+
+            public Car(String brand, String model, int year) {
+                this.brand = brand;
+                this.model = model;
+                this.year = year;
+            }
+
+            public void exibir() {
+                System.out.println("Marca: " + this.brand);
+                System.out.println("Modelo: " + this.model);
+                System.out.println("Ano: " + this.year);
+            }
+        }
+
+        public static void main(String[] args) {
+            Car carro1 = new Car("Fiat", "Palio", 2012);
+            Car carro2 = new Car("Honda", "Civic", 2001);
+
+            System.out.println("Informações do primeiro carro:");
+            carro1.exibir();
+
+            System.out.println("\nInformações do segundo carro:");
+            carro2.exibir();
+        }
+    }
+
+    public static class exercicio14 {
+        public static void main(String[] args) {
+            // Exercício 14: Escreva uma função que receba a temperatura em Fahrenheit e retorne em Celsius.
+            System.out.print("Digite a temperautura em Fahrenheit: ");
+            double temperatura = input.nextDouble();
+
+            double conversor = ((temperatura - 32) * 5) / 9;
+
+            System.out.println(temperatura + "°F em Celsius é " + conversor + "°C");
+        }
+    }
+
+    public static class exercicio15 {
+        public static void main(String[] args) {
+            // Exercício 15: Crie um programa que leia um número inteiro e exiba uma contagem regressiva até 0.
+            System.out.print("Digite um número para contagem regressiva: ");
+            int contagem = input.nextInt();
+
+            for (int i = contagem; i >= 0; i--) {
+                System.out.println(i);
             }
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Checkpoint1_Java_2ESPA2025
+# Checkpoint1 Java 2ESPA2025
+
 Checkpoint 1 (Setembro/2025) - 2ESPA - Domain Driven Design Java
+
+## Professor:
+Carlos Eduardo Machado de Oliveira
+
+## Integrantes do grupo:
+- Gabriel Couto Ribeiro
+- Gabriel Kato Peres
+- João Vitor de Matos Araujo
